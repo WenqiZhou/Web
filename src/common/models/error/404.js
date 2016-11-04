6310732ead94472c217ec6941581612da0d9c262
yeliex <yeliex@yeliex.com>
2016-11-04 23:27:49 +0800

    add fe router

---
+++ src/common/models/error/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ErrorNotFound = () => (
+  <div className="error">
+    404
+  </div>
+);
+
+export default ErrorNotFound;
---
+++ src/common/routers/index.js
@@ -0,0 +1,25 @@
+import React, { PropTypes } from 'react';
+import { Router, Route, IndexRoute, IndexRedirect } from 'react-router';
+import App from '../components/App';
+import { dispatch } from '../store';
+import { WINDOW_CUSTOMER_ROUTE_CLEAR } from '../store/types';
+
+// customerProps提供了一个方法,如果组件中调用了setPath自定义路由路径,必须在Route中加上这个方法清除路径
+const customerProps = {
+  onLeave: () => {
+    dispatch({
+      type: WINDOW_CUSTOMER_ROUTE_CLEAR
+    });
+  }
+};
+
+const Routes = ({ history }) => (
+  <Router history={history} {...customerProps}>
+    <Route path="/" component={App}>
+
+    </Route>
+    <Route path="*" component={require('../models/error/404')} />
+  </Router>
+);
+
+export default Routes;
