013859c6f8f5d4ce13566143180c9a109e53c1ec
yeliex <yeliex@yeliex.com>
2016-11-14 01:02:47 +0800

    add entries

--- src/entries/index.js
+++ src/entries/index.js
@@ -1,5 +1,16 @@
-/**
- * Creator: yeliex
- * Project: 11bnb
- * Description:
- */
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { hashHistory, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import Store from '$common/store';
+import Routes from '$common/routers';
+import '../../views/index.html';
+
+const history = syncHistoryWithStore(process.env.NODE_ENV === 'production' ? browserHistory : hashHistory, Store);
+
+ReactDom.render((
+  <Provider store={Store}>
+    <Routes history={history} />
+  </Provider>
+), document.getElementById('root'));
