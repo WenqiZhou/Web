180dc0eaf5c5d0f04f7d985285f1b01a7f4631b1
yeliex <yeliex@yeliex.com>
2016-11-14 01:05:05 +0800

    add style framework

---
+++ src/common/style/index.js
@@ -0,0 +1,10 @@
+// inport declear
+import './base.less';
+import '../../desktop/style/base.less';
+import '../../mobile/style/base.less';
+
+// inport responsive
+import './responsive.less';
+
+// import icons
+import './theme/default/index.less';
