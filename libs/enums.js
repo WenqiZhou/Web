e1e6b05e207433384caba8807507ad01e1ffca94
yeliex <yeliex@yeliex.com>
2016-11-14 01:01:49 +0800

    add global libs

---
+++ libs/enums.js
@@ -0,0 +1,10 @@
+/* eslint import/prefer-default-export: 0 */
+export const EventType = {
+  0: 'START_TYPE',
+  1: 'ACTIVITY_TYPE',
+  2: 'SEARCH_TYPE',
+  3: 'HOUSE_TYPE',
+  5: 'INVITE_TYPE',
+  6: 'ACTIVITY_NEW_TYPE',
+  7: 'END_TYPE'
+};
