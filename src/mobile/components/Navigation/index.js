0b73a08cb1cc210f9cfc1283149b3996b94acfd9
yeliex <yeliex@yeliex.com>
2016-11-23 02:43:43 +0800

    add navigation component

--- src/mobile/components/Navigation/index.js
+++ src/mobile/components/Navigation/index.js
@@ -8,8 +8,10 @@ export class SearchBar extends Component {
       <nav className="top search-bar">
         <div className="icon mobile menu" />
         <button>
-          <span className="icon mobile search" />
-          想去哪里?
+          <img className="icon" src="/assets/images/icons/icon_mobile_search.png" alt="search" />
+          <span className="description">
+            想去哪里?
+          </span>
         </button>
       </nav>
     )
