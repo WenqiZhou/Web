d35074ab9e0baa3bce18fd0ef76d23d0ed8df9ee
yeliex <yeliex@yeliex.com>
2016-12-08 12:50:57 +0800

    更新输入组件 使支持自定义输入框组,样式以及子组件类名

--- src/common/components/Input/index.js
+++ src/common/components/Input/index.js
@@ -1,3 +1,7 @@
+// 输入框组件 根据移动端/桌面端定制不同的样式
+// 注意 当需要使用Form进行数据绑定的时候 action的子组件千万不能使用Input类型 因为无法绑定
+// Form只能绑定它子一级的组件 需要后续使用decorator重构的时候才能考虑这个问题 不过也没必要考虑太多的需求
+// 用input-group包一层就可以了
 import React, { Component, PropTypes, cloneElement } from 'react';
 import classnames from 'classnames';
 import './index.less';
@@ -8,16 +12,21 @@ export default class Input extends Component {
     label: PropTypes.string,
     clearable: PropTypes.bool,
     handleClear: PropTypes.func,
-    action: PropTypes.node
+    action: PropTypes.node,
+    style: PropTypes.object,
+    className: PropTypes.string,
+    inputClassName: PropTypes.string
   };
 
   render() {
-    const { icon, label, handleClear, clearable, action, ...props } = this.props;
+    const { icon, label, handleClear, clearable, action, style, className = '', ...props } = this.props;
     return (
       <div
+        style={style}
         className={classnames({
           input: true,
-          wrapper: true
+          wrapper: true,
+          [className]: true
         })}
       >
         {
@@ -52,3 +61,12 @@ export default class Input extends Component {
   }
 }
 
+export class Group extends Component {
+  render() {
+    return (
+      <div className="input-group">
+        {this.props.children}
+      </div>
+    )
+  }
+}
