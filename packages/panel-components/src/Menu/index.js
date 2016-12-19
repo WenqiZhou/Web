c27324019586c4c060fd1632c2f5cba041fe658b
yeliex <yeliex@yeliex.com>
2016-12-20 00:10:55 +0800

    增加管理端菜单组件

---
+++ packages/panel-components/src/Menu/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+console.log(1)
+export default class PanelMenu extends Component {
+  render() {
+    return (
+      <div className="menu">
+        menu
+      </div>
+    )
+  }
+}
