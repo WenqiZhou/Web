2de8480e9c6cb2a6b4ca2dd5ecdd76a37661b7fb
yeliex <yeliex@yeliex.com>
2016-12-20 00:10:39 +0800

    增加管理端首页

---
+++ src/panel/modals/host_index/index.js
@@ -0,0 +1,12 @@
+// 房东端首页
+import React, { Component } from 'react';
+
+export default class HostIndex extends Component {
+  render() {
+    return (
+      <div>
+        host index
+      </div>
+    )
+  }
+}
