33a6f2c9b372ffb65b69abb77aebdd6321be3172
yeliex <yeliex@yeliex.com>
2016-12-23 03:55:53 +0800

    添加手机号混淆方法

--- libs/patterns.js
+++ libs/patterns.js
@@ -1,3 +1,9 @@
 export const idCard = /^\d{17}[\d,x,X]$/;
 
 export const phoneNumber = /^1[3|4|5|7|8][0-9]\d{4,8}$/;
+
+export const confuseMobile = (mobile) => {
+  const parsed = String(mobile).replace(/ |\u3000/g, '').match(/(\d{11})$/);
+
+  return parsed ? parsed[0].replace(/^(\d{3})\d{4}(\d{4})$/, (...r) => `${r[1]}****${r[2]}`) : mobile
+};
