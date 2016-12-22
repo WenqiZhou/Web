b6b2cc6056abc8a8bd2cb4a3b4dc61f5ac0cc16c
yeliex <yeliex@yeliex.com>
2016-12-23 03:55:37 +0800

    添加本地mock数据

---
+++ libs/mock.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+
+export const UserInfo = {
+  "header": {
+    "client_ip": "218.72.17.189",
+    "access_ip": "10.11.254.235",
+    "client_uversion": 50331904,
+    "user_uin": 2114320527,
+    "uni_id": "ab01b39c-c875-11e6-a895-02355002f52f",
+    "client_version": "3.0.1.0"
+  },
+  "user": {
+    "clt_type": 1,
+    "igetui_cltid": "1e86db01479e15eec916aec51e20e4aa",
+    "updated_at": 1477454867,
+    "shake_feedback": 2,
+    "uin": 2114320527,
+    "new_verified_school_email": 2,
+    "tutorial": 100,
+    "head_url": "http://s3.cn-north-1.amazonaws.com.cn/11bnbimgs/b2b07126-9b31-11e6-8697-02355002f52f.u",
+    "share_url": "http://h5.11bnb.com/app/share/userInfo/2114320527",
+    "wanted_block": 2,
+    "hx_pwd": "CpANw3hRr5Iz",
+    "reply_rate": -1.0,
+    "hx_id": "1362775179",
+    "average_reply_time": -1,
+    "thumbImage": "http://s3.cn-north-1.amazonaws.com.cn/11bnbstatic/invite_logo.png",
+    "phone": "+8615988545765",
+    "phone_verified": true,
+    "sms_block": 2,
+    "member_number": "TJ073222",
+    "school": "\u5b81\u6ce2\u804c\u4e1a\u6280\u672f\u5b66\u9662",
+    "is_in_white_list": 2,
+    "push_block": 2,
+    "gender": 1,
+    "created_at": 1477454791,
+    "real_name": "yeliex",
+    "receptance_rate": -1.0
+  }
+};
