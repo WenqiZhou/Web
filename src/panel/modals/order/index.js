6b1a472373ec7fba9a9d238d7100356a52be364f
yeliex <yeliex@yeliex.com>
2016-12-26 03:28:08 +0800

    添加订单详情页面

---
+++ src/panel/modals/order/index.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import classnames from 'classnames';
+import { Card } from '11-panel';
+import { Row } from '11-common';
+import Style from './index.less';
+
+export default class OrderDetail extends Component {
+  render() {
+    return (
+      <div className={Style.order}>
+        <Row>
+          <Card className={Style.path}>
+            {/* eslint react/no-unescaped-entities: 0 */}
+            我是房客 > 我的订单 > 订单管理 > 订单详情
+          </Card>
+        </Row>
+        <Row style={{ margin: '-.08rem 0 .27rem' }}>
+          <Card className={Style.status}>
+            当前订单状态：已完成
+            <span className="right">取消订单</span>
+          </Card>
+        </Row>
+        <Row>
+          <Card className={Style.detail}>
+            <div className={Style.title}>
+              预订信息（订单号：34234343）
+              <span className="right">下单时间：2016-04-01 16:38:01</span>
+            </div>
+            xxxx
+          </Card>
+        </Row>
+        <Row>
+          <Card className={Style.detail}>
+            <div className={classnames(Style.title)}>
+              订单总额
+              <span className="right orange">¥242</span>
+            </div>
+            <div className={classnames(Style.title, 'normal')}>
+              房租：
+              <span className="right orange">¥142</span>
+            </div>
+            <div className={classnames(Style.title, 'normal')}>
+              押金：
+              <span className="right orange">¥100</span>
+            </div>
+          </Card>
+        </Row>
+        <Row>
+          <Card className={Style.detail}>
+            <div className={classnames(Style.title)}>
+              预定人信息
+            </div>
+            <div style={{ lineHeight: '.6rem' }} className={classnames(Style.title, 'normal')}>
+              <dl>
+                <dt>预定人</dt>
+                <dd>赵铁柱</dd>
+                <dt>手机号</dt>
+                <dd>15632324455</dd>
+                <dt>身份证</dt>
+                <dd>11044545156</dd>
+              </dl>
+            </div>
+          </Card>
+        </Row>
+        <Row>
+          <Card className={Style.detail}>
+            <div className={classnames(Style.title)}>
+              退订政策
+              <span className="orange right">适中</span>
+            </div>
+            xxx
+          </Card>
+        </Row>
+      </div>
+    )
+  }
+}
--- src/panel/routers/order.js
+++ src/panel/routers/order.js
@@ -1,10 +1,8 @@
+// 订单相关的路由
 import React, { PropTypes } from 'react';
 import { Router, Route, IndexRoute, IndexRedirect } from 'react-router';
-import App from '$common/components/App';
 import { dispatch } from '$common/store';
 import { WINDOW_CUSTOMER_ROUTE_CLEAR } from '$common/store/types';
-import { Menu } from '11-panel';
-import Routers from './routers';
 
 // customerProps提供了一个方法,如果组件中调用了setPath自定义路由路径,必须在Route中加上这个方法清除路径
 const customerProps = {
@@ -14,16 +12,11 @@ const customerProps = {
     });
   }
 };
+
 const Routes = (
-  <Route path="/dashboard" component={require('../components/App')}>
-    <IndexRedirect to="orders" />
-    {Routers.orders}
-    {Routers.order}
-    <Route path="host">
-      {Routers.orders}
-      {Routers.order}
-      <IndexRoute component={require('../modals/host_index')} />
-    </Route>
+  <Route path="order" component={require('../modals/order')}>
+    <IndexRedirect to="/dashboard/" />
+    <Route path=":id" component={require('../modals/order')} />
   </Route>
 );
 
