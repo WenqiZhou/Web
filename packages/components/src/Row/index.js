538e4cd3614a9d01b30ac84652a21c849c313554
yeliex <yeliex@yeliex.com>
2016-12-26 03:27:40 +0800

    为Row组件添加样式与class支持

--- packages/components/src/Row/index.js
+++ packages/components/src/Row/index.js
@@ -6,13 +6,15 @@ import './index.less';
 export default class Row extends Component {
   static propTypes = {
     // 在Row中加入space来代表Row中每个组件之间的间距,与Col中的offset每个组件右移不同
-    space: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+    space: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    style: PropTypes.object,
+    className: PropTypes.string
   };
 
   render() {
     const children = this.props.children instanceof Array ? this.props.children : [this.props.children];
     return (
-      <div className="row">
+      <div style={this.props.style} className={`row ${this.props.className || ''}`}>
         {
           children.map((child, index) => cloneElement(child, {
             key: index,
