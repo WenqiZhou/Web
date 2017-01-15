c1dbf26d58d9fa6dee8e24c7a654feb704b052a3
yeliex <yeliex@yeliex.com>
2017-01-15 23:02:48 +0800

    add initial state for serverrending

--- src/common/store/index.js
+++ src/common/store/index.js
@@ -1,3 +1,4 @@
+/* eslint no-underscore-dangle: 0 */
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { hashHistory, browserHistory } from 'react-router';
 import { routerReducer as routing, routerMiddleware } from 'react-router-redux';
@@ -9,7 +10,7 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(combineReducers({
   ...reducers, routing
-}), {}, compose(
+}), typeof window === 'object' ? window.__INITIAL_STATE__ || {} : {}, compose(
   applyMiddleware(sagaMiddleware, routerMiddleware(process.env.NODE_ENV === 'production' || process.env.history === 'browserHistory' ? browserHistory : hashHistory)),
   typeof window !== 'undefined' && window.devToolsExtension ? window.devToolsExtension() : f => f // 调用redux-devtools-extension
 ));
